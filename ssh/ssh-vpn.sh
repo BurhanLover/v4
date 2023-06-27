fde9de89567b20d14d291296f38380aa5203e327
Andyyuda <128485068+Andyyuda@users.noreply.github.com>
2023-06-27 17:43:43 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -321,7 +321,7 @@ wget -O acs-set "https://raw.githubusercontent.com/Andyyuda/v4/main/acs-set.sh"
 
 wget -O sshws "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/sshws.sh"
 wget -O status "https://raw.githubusercontent.com/Andyyuda/v4/main/status.sh"
-wget -O menu-backup "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/menu-backup.sh"
+wget -O menu-bckp "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/menu-bckp.sh"
 wget -O backup "https://raw.githubusercontent.com/Andyyuda/v4/main/backup/backup.sh"
 wget -O restore "https://raw.githubusercontent.com/Andyyuda/v4/main/backup/restore.sh"
 
@@ -375,7 +375,7 @@ chmod +x xp
 chmod +x acs-set
 chmod +x sshws
 chmod +x status
-chmod +x menu-backup
+chmod +x menu-bckp
 chmod +x backup
 chmod +x restore
 chmod +x jam
