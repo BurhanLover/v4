ce749b3e0f145735f50789f92845161db63bfd63
Andyyuda <128485068+Andyyuda@users.noreply.github.com>
2023-07-26 01:32:58 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -253,13 +253,7 @@ export sem=$( curl -s https://raw.githubusercontent.com/artanodrop/permission/ma
 export pak=$( cat /home/.ver)
 IPVPS=$(curl -s ipinfo.io/ip )
 clear
-echo ""
-echo -e "$green                   ╔╗╔═╗╔╗   ╔═╗╔═╗╔═══╗╔╗╔═╗$NC"
-echo -e "$green                   ║║║╔╝║║   ║║╚╝║║║╔═╗║║║║╔╝$NC"
-echo -e "$green                   ║╚╝╝ ║║   ║╔╗╔╗║║╚═╝║║╚╝╝ $NC"
-echo -e "$green                   ║╔╗║ ║║ ╔╗║║║║║║║╔══╝║╔╗║ $NC"
-echo -e "$green                   ║║║╚╗║╚═╝║║║║║║║║║   ║║║╚╗$NC"
-echo -e "$green                   ╚╝╚═╝╚═══╝╚╝╚╝╚╝╚╝   ╚╝╚═╝$NC"
+echo -e "$green                 ▌║█║▌│║▌│║▌║▌█║KLMPK ▌│║▌║▌│║║▌█║▌║█ ${NC}"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "\E[44;1;39m                     ⇱ INFORMASI VPS ⇲                        \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
