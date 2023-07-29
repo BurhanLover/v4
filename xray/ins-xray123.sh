29bf2af65b75a313a954b111bcfae8fb54aaa7ec
Andyyuda <128485068+Andyyuda@users.noreply.github.com>
2023-07-29 23:01:53 +0700

    Update ins-xray123.sh

--- xray/ins-xray123.sh
+++ xray/ins-xray123.sh
@@ -4,24 +4,24 @@ echo -e "
 date
 echo ""
 domain=$(cat /root/domain)
-sleep 1
+sleep 0.5
 mkdir -p /etc/xray 
 echo -e "[ ${green}INFO${NC} ] Checking... "
 apt install iptables iptables-persistent -y
-sleep 1
+sleep 0.5
 echo -e "[ ${green}INFO$NC ] Setting ntpdate"
 ntpdate pool.ntp.org 
 timedatectl set-ntp true
-sleep 1
+sleep 0.5
 echo -e "[ ${green}INFO$NC ] Enable chronyd"
 systemctl enable chronyd
 systemctl restart chronyd
-sleep 1
+sleep 0.5
 echo -e "[ ${green}INFO$NC ] Enable chrony"
 systemctl enable chrony
 systemctl restart chrony
 timedatectl set-timezone Asia/Jakarta
-sleep 1
+sleep 0.5
 echo -e "[ ${green}INFO$NC ] Setting chrony tracking"
 chronyc sourcestats -v
 chronyc tracking -v
@@ -36,23 +36,31 @@ apt install curl pwgen openssl netcat cron -y
 
 
 # install xray
-sleep 1
+sleep 0.5
 echo -e "[ ${green}INFO$NC ] Downloading & Installing xray core"
 domainSock_dir="/run/xray";! [ -d $domainSock_dir ] && mkdir  $domainSock_dir
 chown www-data.www-data $domainSock_dir
 # Make Folder XRay
+mkdir -p /etc/{bot,xray,vmess,websocket,vless,trojan,shadowsocks}
 mkdir -p /var/log/xray
 mkdir -p /etc/xray
+mkdir -p /etc/geostore/
 chown www-data.www-data /var/log/xray
 chmod +x /var/log/xray
 touch /var/log/xray/access.log
 touch /var/log/xray/error.log
 touch /var/log/xray/access2.log
 touch /var/log/xray/error2.log
+touch /var/log/xray/{access.log,error.log}
+chmod 777 /var/log/xray/*.log
+touch /etc/bot/.bot.db
+touch /etc/vmess/.vmess.db
+touch /etc/vless/.vless.db
+touch /etc/trojan/.trojan.db
+touch /etc/ssh/.ssh.db
+touch /etc/shadowsocks/.shadowsocks.db
 # / / Ambil Xray Core Version Terbaru
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.5.6
-
-
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.6.1
 
 ## crt xray
 systemctl stop nginx
@@ -74,7 +82,30 @@ echo -n '#!/bin/bash
 chmod +x /usr/local/bin/ssl_renew.sh
 if ! grep -q 'ssl_renew.sh' /var/spool/cron/crontabs/root;then (crontab -l;echo "15 03 */3 * * /usr/local/bin/ssl_renew.sh") | crontab;fi
 
+# make folder xray
+rm -rf /etc/vmess/.vmess.db
+rm -rf /etc/vless/.vless.db
+rm -rf /etc/trojan/.trojan.db
+rm -rf /etc/shadowsocks/.shadowsocks.db
+rm -rf /etc/ssh/.ssh.db
+mkdir -p /etc/xray
+mkdir -p /etc/vmess
+mkdir -p /etc/vless
+mkdir -p /etc/trojan
+mkdir -p /etc/shadowsocks
+mkdir -p /usr/bin/xray/
+mkdir -p /var/log/xray/
+mkdir -p /var/www/html
 mkdir -p /home/vps/public_html
+chmod +x /var/log/xray
+touch /etc/xray/domain
+touch /var/log/xray/access.log
+touch /var/log/xray/error.log
+touch /etc/vmess/.vmess.db
+touch /etc/vless/.vless.db
+touch /etc/trojan/.trojan.db
+touch /etc/shadowsocks/.shadowsocks.db
+touch /etc/ssh/.ssh.db
 
 # set uuid
 uuid=$(cat /proc/sys/kernel/random/uuid)
@@ -84,7 +115,7 @@ cat > /etc/xray/config.json << END
   "log" : {
     "access": "/var/log/xray/access.log",
     "error": "/var/log/xray/error.log",
-    "loglevel": "warning"
+    "loglevel": "info"
   },
   "inbounds": [
       {
@@ -369,120 +400,14 @@ Restart=on-abort
 WantedBy=multi-user.target
 EOF
 
-# Install Trojan Go
-latest_version="$(curl -s "https://api.github.com/repos/p4gefau1t/trojan-go/releases" | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
-trojango_link="https://github.com/p4gefau1t/trojan-go/releases/download/v${latest_version}/trojan-go-linux-amd64.zip"
-mkdir -p "/usr/bin/trojan-go"
-mkdir -p "/etc/trojan-go"
-cd `mktemp -d`
-curl -sL "${trojango_link}" -o trojan-go.zip
-unzip -q trojan-go.zip && rm -rf trojan-go.zip
-mv trojan-go /usr/local/bin/trojan-go
-chmod +x /usr/local/bin/trojan-go
-mkdir /var/log/trojan-go/
-touch /etc/trojan-go/akun.conf
-touch /var/log/trojan-go/trojan-go.log
-
-# Buat Config Trojan Go
-cat > /etc/trojan-go/config.json << END
-{
-  "run_type": "server",
-  "local_addr": "0.0.0.0",
-  "local_port": 2087,
-  "remote_addr": "127.0.0.1",
-  "remote_port": 89,
-  "log_level": 1,
-  "log_file": "/var/log/trojan-go/trojan-go.log",
-  "password": [
-      "$uuid"
-  ],
-  "disable_http_check": true,
-  "udp_timeout": 60,
-  "ssl": {
-    "verify": false,
-    "verify_hostname": false,
-    "cert": "/etc/xray/xray.crt",
-    "key": "/etc/xray/xray.key",
-    "key_password": "",
-    "cipher": "",
-    "curves": "",
-    "prefer_server_cipher": false,
-    "sni": "$domain",
-    "alpn": [
-      "http/1.1"
-    ],
-    "session_ticket": true,
-    "reuse_session": true,
-    "plain_http_response": "",
-    "fallback_addr": "127.0.0.1",
-    "fallback_port": 0,
-    "fingerprint": "firefox"
-  },
-  "tcp": {
-    "no_delay": true,
-    "keep_alive": true,
-    "prefer_ipv4": true
-  },
-  "mux": {
-    "enabled": false,
-    "concurrency": 8,
-    "idle_timeout": 60
-  },
-  "websocket": {
-    "enabled": true,
-    "path": "/trojango",
-    "host": "$domain"
-  },
-    "api": {
-    "enabled": false,
-    "api_addr": "",
-    "api_port": 0,
-    "ssl": {
-      "enabled": false,
-      "key": "",
-      "cert": "",
-      "verify_client": false,
-      "client_cert": []
-    }
-  }
-}
-END
-
-# Installing Trojan Go Service
-cat > /etc/systemd/system/trojan-go.service << END
-[Unit]
-Description=Trojan-Go Service Mod By Tarap-Kuhing
-Documentation=github.com/adammoi/vipies
-After=network.target nss-lookup.target
-
-[Service]
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/local/bin/trojan-go -config /etc/trojan-go/config.json
-Restart=on-failure
-RestartPreventExitStatus=23
-
-[Install]
-WantedBy=multi-user.target
-END
-
-# Trojan Go Uuid
-cat > /etc/trojan-go/uuid.txt << END
-$uuid
-END
-
 #nginx config
 cat >/etc/nginx/conf.d/xray.conf <<EOF
     server {
              listen 80;
              listen [::]:80;
-             listen 8080;
-             listen [::]:8080;
              listen 443 ssl http2 reuseport;
              listen [::]:443 http2 reuseport;	
-             server_name $domain;
+             server_name *.$domain;
              ssl_certificate /etc/xray/xray.crt;
              ssl_certificate_key /etc/xray/xray.key;
              ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
@@ -518,18 +443,6 @@ sed -i '$ ilocation = /trojan-ws' /etc/nginx/conf.d/xray.conf
 sed -i '$ i{' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_redirect off;' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_pass http://127.0.0.1:25432;' /etc/nginx/conf.d/xray.conf
-sed -i '$ iproxy_http_version 1.1;' /etc/nginx/conf.d/xray.conf
-sed -i '$ iproxy_set_header X-Real-IP \$remote_addr;' /etc/nginx/conf.d/xray.conf
-sed -i '$ iproxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;' /etc/nginx/conf.d/xray.conf
-sed -i '$ iproxy_set_header Upgrade \$http_upgrade;' /etc/nginx/conf.d/xray.conf
-sed -i '$ iproxy_set_header Connection "upgrade";' /etc/nginx/conf.d/xray.conf
-sed -i '$ iproxy_set_header Host \$http_host;' /etc/nginx/conf.d/xray.conf
-sed -i '$ i}' /etc/nginx/conf.d/xray.conf
-
-sed -i '$ ilocation = /trojango' /etc/nginx/conf.d/xray.conf
-sed -i '$ i{' /etc/nginx/conf.d/xray.conf
-sed -i '$ iproxy_redirect off;' /etc/nginx/conf.d/xray.conf
-sed -i '$ iproxy_pass http://127.0.0.1:2087;' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_http_version 1.1;' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_set_header X-Real-IP \$remote_addr;' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;' /etc/nginx/conf.d/xray.conf
@@ -589,10 +502,18 @@ sed -i '$ igrpc_set_header Host \$http_host;' /etc/nginx/conf.d/xray.conf
 sed -i '$ igrpc_pass grpc://127.0.0.1:33456;' /etc/nginx/conf.d/xray.conf
 sed -i '$ i}' /etc/nginx/conf.d/xray.conf
 
+sed -i '$ ilocation ^~ /ss-grpc' /etc/nginx/conf.d/xray.conf
+sed -i '$ i{' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_redirect off;' /etc/nginx/conf.d/xray.conf
+sed -i '$ igrpc_set_header X-Real-IP \$remote_addr;' /etc/nginx/conf.d/xray.conf
+sed -i '$ igrpc_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;' /etc/nginx/conf.d/xray.conf
+sed -i '$ igrpc_set_header Host \$http_host;' /etc/nginx/conf.d/xray.conf
+sed -i '$ igrpc_pass grpc://127.0.0.1:30310;' /etc/nginx/conf.d/xray.conf
+sed -i '$ i}' /etc/nginx/conf.d/xray.conf
 
 echo -e "$yell[SERVICE]$NC Restart All service"
 systemctl daemon-reload
-sleep 1
+sleep 0.5
 echo -e "[ ${green}ok${NC} ] Enable & restart xray "
 systemctl daemon-reload
 systemctl enable xray
@@ -600,11 +521,61 @@ systemctl restart xray
 systemctl restart nginx
 systemctl enable runn
 systemctl restart runn
-systemctl stop trojan-go
-systemctl start trojan-go
-systemctl enable trojan-go
-systemctl restart trojan-go
-sleep 1
+
+cd /usr/bin/
+# vmess
+echo -e "${Green}[ INFO ]${NC} ${Green}Downloading Main Vmess${NC}"
+wget -O addws "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/addws.sh" && chmod +x addws
+wget -O trialws "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/trialws.sh" && chmod +x trialws
+wget -O renewws "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/renewws.sh" && chmod +x renewws
+wget -O delws "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/delws.sh" && chmod +x delws
+wget -O cek-ws "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/cek-ws.sh" && chmod +x cek-ws
+sleep 0.5
+# vless
+echo -e "${Green}[ INFO ]${NC} ${Green}Downloading Main Vless${NC}"
+wget -O add-vless "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/add-vless.sh" && chmod +x add-vless
+wget -O trialvless "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/trialvless.sh" && chmod +x trialvless
+wget -O renew-vless "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/renew-vless.sh" && chmod +x renew-vless
+wget -O del-vless "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/del-vless.sh" && chmod +x del-vless
+wget -O cek-vless "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/cek-vless.sh" && chmod +x cek-vless
+sleep 0.5
+# trojan
+echo -e "${Green}[ INFO ]${NC} ${Green}Downloading Main Trojan${NC}"
+wget -O add-tr "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/add-tr.sh" && chmod +x add-tr
+wget -O trialtrojan "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/trialtrojan.sh" && chmod +x trialtrojan
+wget -O del-tr "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/del-tr.sh" && chmod +x del-tr
+wget -O renew-tr "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/renew-tr.sh" && chmod +x renew-tr
+wget -O cek-tr "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/cek-tr.sh" && chmod +x cek-tr
+sleep 0.5
+# shadowsocks
+echo -e "${Green}[ INFO ]${NC} ${Green}Downloading Main shadowsoks${NC}"
+wget -O add-ssws "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/add-ssws.sh" && chmod +x add-ssws
+wget -O trialssws "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/trialssws.sh" && chmod +x trialssws
+wget -O del-ssws "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/del-ssws.sh" && chmod +x del-ssws
+wget -O renew-ssws "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/renew-ssws.sh" && chmod +x renew-ssws
+wget -O tcp "https://raw.githubusercontent.com/Andyyuda/supreme/main/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
+wget -O nf "https://raw.githubusercontent.com/Andyyuda/nonton/main/nf.sh" && chmod +x nf.sh && ./nf.sh
+sleep 0.5
+#Sistem Tambahan
+wget -O menu-tambah "https://raw.githubusercontent.com/Andyyuda/begeg/main/menu-tambah.sh" && chmod +x menu-tambah
+wget -O global "https://raw.githubusercontent.com/Andyyuda/gelo/main/gelo.sh" && chmod +x global
+wget -O sc "https://raw.githubusercontent.com/Andyyuda/begeg/main/sc.sh" && chmod +x sc
+wget -O killtrial "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/killtrial.sh" && chmod +x killtrial
+wget -O jilmek "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/jilmek.sh" && chmod +x jilmek
+wget -O bdsm "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/bdsm.sh" && chmod +x bdsm
+wget -O limiter "https://raw.githubusercontent.com/Andyyuda/supreme/main/xray/limiter.sh" && chmod +x limiter
+sleep 0.5
+#Bot menu
+wget -O bot-cek-tr "https://raw.githubusercontent.com/Andyyuda/bot/main/bot-cek-tr.sh" && chmod +x bot-cek-tr
+wget -O bot-cek-vless "https://raw.githubusercontent.com/Andyyuda/bot/main/bot-cek-vless.sh" && chmod +x bot-cek-vless
+wget -O bot-cek-ws "https://raw.githubusercontent.com/Andyyuda/bot/main/bot-cek-wd.sh" && chmod +x bot-cek-ws
+wget -O restorebot "https://raw.githubusercontent.com/Andyyuda/bot/main/restorebot.sh" && chmod +x restorebot
+wget -O backupbot "https://raw.githubusercontent.com/Andyyuda/bot/main/backupbot.sh" && chmod +x backupbot
+wget -O menu-bot "https://raw.githubusercontent.com/Andyyuda/bot/main/menu-bot.sh" && chmod +x menu-bot
+wget -O bot-cek-ssws "https://raw.githubusercontent.com/Andyyuda/bot/main/bot-cek-ssws.sh" && chmod +x bot-cek-ssws
+wget -O backupbot "https://raw.githubusercontent.com/Andyyuda/bot/main/backupbot.sh" && chmod +x backupbot
+wget -O restorebot "https://raw.githubusercontent.com/Andyyuda/supreme/main/restorebot.sh" && chmod +x restorebot
+sleep 0.5
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 yellow "xray/Vmess"
 yellow "xray/Vless"
