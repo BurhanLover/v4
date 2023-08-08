83be1ad86ee55e91804dcb5e5e1726b7f00c1d99
BurhanLover <141700706+BurhanLover@users.noreply.github.com>
2023-08-08 11:14:02 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/Andyyuda/permission/main/ipmini > /root/tmp
+    curl -sS https://raw.githubusercontent.com/BurhanLover/permission/main/ipmini > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -19,7 +19,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/Andyyuda/permission/main/ipmini | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/BurhanLover/permission/main/ipmini | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -36,7 +36,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/Andyyuda/permission/main/ipmini | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/BurhanLover/permission/main/ipmini | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
@@ -54,7 +54,7 @@ PERMISSION
 if [ "$res" = "Expired" ]; then
 Exp="\e[36mExpired\033[0m"
 else
-Exp=$(curl -sS https://raw.githubusercontent.com/Andyyuda/permission/main/ipmini | grep $MYIP | awk '{print $3}')
+Exp=$(curl -sS https://raw.githubusercontent.com/BurhanLover/permission/main/ipmini | grep $MYIP | awk '{print $3}')
 fi
 
 # =========================================
@@ -249,7 +249,7 @@ echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
 }
-export sem=$( curl -s https://raw.githubusercontent.com/Andyyuda/v4/main/versions)
+export sem=$( curl -s https://raw.githubusercontent.com/BurhanLover/v4/main/versions)
 export pak=$( cat /home/.ver)
 IPVPS=$(curl -s ipinfo.io/ip )
 clear
@@ -269,16 +269,16 @@ echo -e "${BIYellow}□ Clients Name        = ${GREEN}$Name${NC}"
 echo -e "${BIYellow}□ Expired Script VPS  = ${GREEN}$Exp${NC}"
 echo -e "${BIYellow}□ Time Reboot VPS     = 00:00 ${GREEN}( Jam 12 Malam )${NC}"
 echo -e "${BIYellow}□ License Limit       = ORDER ${GREEN}( 082131861788 )${NC}"
-echo -e "${BIYellow}□ AutoScript By Dev   = Wak ${GREEN}( Andyyuda )${NC}"
+echo -e "${BIYellow}□ AutoScript By Dev   = Wak ${GREEN}( BurhanLover )${NC}"
 echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
-echo -e "${BICyan} │                  ${BIWhite}${UWhite}ANDYYUDA TUNELING${NC}"
+echo -e "${BICyan} │                  ${BIWhite}${UWhite}BurhanLover TUNELING${NC}"
 echo -e "${BICyan} │"
 echo -e " ${BICyan}│  ${BICyan}Use Core        :  ${BIPurple}XRAY${NC}"
 echo -e " ${BICyan}│  ${BICyan}IP-VPS          :  ${BIYellow}$IPVPS${NC}"
 echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
 
-echo -e "${GREEN}  INGATLAH KAWAN DILANGIT MASIH ADA LANGIT JAGAN LUPA DIRI${NC}" 
+echo -e "${GREEN}  INGATLAH KAWAN DILANGIT MASIH ADA LATO-LATO${NC}" 
 
 echo -e "${GREEN}┌──────────────────────────────────────────────────┐${NC}" 
 echo -e "${GREEN}│  \033[0m ${BOLD}${YELLOW}SSH     VMESS       VLESS      TROJAN $NC" 
@@ -300,7 +300,7 @@ echo -e "     ${BICyan}[${BIWhite}12${BICyan}] lock ssh "
 echo -e "     ${BICyan}[${BIWhite}13${BICyan}] unlock ssh "
 echo -e "     ${BICyan}[${BIWhite}14${BICyan}] Update Script "  
 echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
-echo -e "\E[44;1;39m     ⇱ ▌║█║▌│║▌│║▌║▌█║KLMPK ▌│║▌║▌│║║▌█║▌║█⇲                         \E[0m"
+echo -e "\E[44;1;39m     ⇱ ▌║█║▌│║▌│║▌║▌█║BurhanLover ▌│║▌║▌│║║▌█║▌║█⇲                         \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
 echo -e "${BICyan} │$NC ${BICyan}HARI ini${NC}: ${red}$ttoday$NC ${BICyan}KEMARIN${NC}: ${red}$tyest$NC ${BICyan}BULAN${NC}: ${red}$tmon$NC $NC"
